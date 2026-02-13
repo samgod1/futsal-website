2d2f04a2dd494bc78020da9ff34b55e4e3723e68
SamGod69 <sampannatamrakar@gmail.com>
2026-02-13 14:01:03 +0545

    Completed quick stats and quick details section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,3 +1,7 @@
+import { FaLocationCrosshairs } from "react-icons/fa6";
+import { MdAccessTimeFilled } from "react-icons/md";
+import { FaMoneyBill, FaPhoneAlt } from "react-icons/fa";
+
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 
@@ -5,7 +9,7 @@ const Home = () => {
 	return (
 		<div className="home-page">
 			<Navbar />
-			<section className="hero-section">
+			<section className="hero">
 				<div className="img">
 					<img src="/images/futsal.png" alt="futsal" />
 				</div>
@@ -17,6 +21,56 @@ const Home = () => {
 						taps away. Grab your squad, lace up, and let's play!
 					</p>
 					<button>Book now</button>
+				</div>
+			</section>
+			<section className="quick-stats">
+				<div className="stat">
+					<h3>+10k</h3>
+					<p>Total users</p>
+				</div>
+				<div className="stat">
+					<h3>+35k</h3>
+					<p>Bookings made</p>
+				</div>
+			</section>
+			<hr />
+			<section className="quick-info">
+				<div className="info">
+					<div className="text-group">
+						<h3>
+							<MdAccessTimeFilled />
+							Time
+						</h3>
+						<p>8:00am-8:00pm</p>
+					</div>
+					<div className="text-group">
+						<h3>
+							<FaLocationCrosshairs />
+							Location
+						</h3>
+						<p>Paknajol, Kathmandu</p>
+					</div>
+					<div className="text-group">
+						<h3>
+							<FaMoneyBill />
+							Pricing
+						</h3>
+						<p>
+							Sun-Fri: Rs 1200
+							<br />
+							Sat: Rs 1500
+						</p>
+					</div>
+					<div className="text-group">
+						<h3>
+							<FaPhoneAlt />
+							Contact
+						</h3>
+						<p>9818229900</p>
+					</div>
+				</div>
+				<div className="wrapper">
+					<img src="/images/playing-with-ball.jpg" alt="playing-with-ball" />
 				</div>
 			</section>
 		</div>
